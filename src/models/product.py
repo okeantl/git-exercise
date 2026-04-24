8a9c7d45e5a8c24eaab5917171d5c53d5b5c06a5
valera <valera-0099@yandex.ru>
2026-04-24 15:46:43 +0300

    вернул файл, так как он был пустым

--- src/models/product.py
+++ src/models/product.py
@@ -21,10 +21,5 @@ class Product():
             return False
         return self.name == other.name and self.price == other.price
 
-<<<<<<< HEAD
 # ! dawhbfga
-=======
-
->>>>>>> vetka-conflict
 # ! добавил коментарий для конфликта
-# ! dawhbfga
